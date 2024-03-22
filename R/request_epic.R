653454d4a81350f22626e5da7bd13b8f4960660f
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2024-03-22 15:22:11 +0100

    change request

--- R/request_epic.R
+++ R/request_epic.R
@@ -1,10 +1,11 @@
 #' Title
 #'
-#' @param route
-#' @return
+#' @param epic_id The identifier of an ePIC object
+#' @return Requested information about an ePIC object
 #'
-request_epic <- function(route) {
-  path <- with_host(route)
+request_epic <- function(epic_id) {
+  path <-
+    paste("https://doi.org/", epic_id, "?locatt=view:json", sep = "")
   req <- httr2::request(path)
   resp <- httr2::req_perform(req)
   info <- httr2::resp_body_json(resp)
--- tests/testthat/test-request_epic.R
+++ tests/testthat/test-request_epic.R
@@ -1,3 +1,4 @@
-test_that("request_epic gets information from epicdocument(", {
-  expect_equal(2 * 2, 4)
+test_that("request_epic gets info from epic object", {
+  info <- request_epic("21.T11969/6128ce5def6ffac006e0")
+  expect_equal(info$name, "B2INST-Schema")
 })
