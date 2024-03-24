dfb0beb8cba40811d7b66317fa2884a8a5996017
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2024-03-24 20:16:01 +0100

    lintr

--- R/extract_epic.R
+++ R/extract_epic.R
@@ -3,8 +3,7 @@
 #' @param epic_id The identifier of an ePIC object
 #' @return Boolean, whether the file is in the extdata
 #'
-check_static <- function(epic_id)
-{
+check_static <- function(epic_id) {
   id_1 <- strsplit(epic_id, split = "/")[[1]][1]
   name <- paste(id_1, ".json", sep = "")
   all_static <-
