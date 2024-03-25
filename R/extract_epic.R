ffeece09a32f44df714360ea4c093dbde53a3c40
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2024-03-25 22:01:34 +0100

    extract_epic and test

--- R/extract_epic.R
+++ R/extract_epic.R
@@ -11,3 +11,39 @@ check_static <- function(epic_id) {
   checked <- name %in% all_static
   return(checked)
 }
+
+#' Title
+#'
+#' @param epic_id The identifier of an ePIC object
+#' @return An R object that contains information about the ePIC object
+#'
+extract_epic <- function(epic_id) {
+  extract_all <- list()
+  extractor_function <- function(epic_id) {
+    info <- request_epic(epic_id)
+    name <- info$name
+    identifier <- info$Identifier
+    schema_df <- data.frame(name, identifier)
+    all_props <- list()
+    if (info$Schema$Type == "Object" |
+        info$Schema$Type == "Array") {
+      for (prop in info$Schema$Properties) {
+        specific_prop <- list()
+        specific_prop[["predicate_label"]] <- prop$Name
+        specific_prop[["nested_name"]] <- prop$Type
+        specific_prop[["cardinality"]] <-
+          prop$Properties$Cardinality
+        if (!is.null(prop$Type) &
+            !"nested_name" %in% names(extract_all)) {
+          extractor_function(prop$Type)
+        }
+        all_props <- append(all_props, list(specific_prop))
+      }
+    }
+    extracted <- list(schema_df, all_props)
+    extract_all[[name]] <<- list(extracted)
+    return(extract_all)
+  }
+  extractor_function(epic_id)
+  return(extract_all)
+}
--- tests/testthat/test-extract_epic.R
+++ tests/testthat/test-extract_epic.R
@@ -1,3 +1,15 @@
 test_that("check_static detects an absence of a file in the extdata", {
   expect_equal(check_static("abcd/abcd"), FALSE)
 })
+
+test_that("extract_epic gives the specified template", {
+  result_extract_epic <- extract_epic("21.T11969/1ea0e148d9bbe08335cd")
+  string_representation <- capture.output(print(result_extract_epic))
+  expected <- c("$`PIDINST-SchemaObject`",                               "$`PIDINST-SchemaObject`[[1]]",
+                "$`PIDINST-SchemaObject`[[1]][[1]]",                     "                  name                     identifier",
+                "1 PIDINST-SchemaObject 21.T11969/1ea0e148d9bbe08335cd", "",
+                "$`PIDINST-SchemaObject`[[1]][[2]]",                     "list()",
+                "",                                                      "",
+                ""    )
+  expect_equal(string_representation, expected)
+})
