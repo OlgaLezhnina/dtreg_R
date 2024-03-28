7fd83a456e9f635817ff5487e0e4ae11fc437bc8
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2024-03-28 18:24:32 +0100

    move check_static and its test to separate files

--- R/extract_epic.R
+++ R/extract_epic.R
@@ -1,20 +1,6 @@
 #' Title
 #'
 #' @param epic_id The identifier of an ePIC object
-#' @return Boolean, whether the file is in the extdata
-#'
-check_static <- function(epic_id) {
-  id_1 <- strsplit(epic_id, split = "/")[[1]][1]
-  name <- paste(id_1, ".json", sep = "")
-  all_static <-
-    system.file("extdata", package = "dtreg") |> list.files()
-  checked <- name %in% all_static
-  return(checked)
-}
-
-#' Title
-#'
-#' @param epic_id The identifier of an ePIC object
 #' @return An R object that contains information about the ePIC object
 #'
 extract_epic <- function(epic_id) {
--- tests/testthat/test-extract_epic.R
+++ tests/testthat/test-extract_epic.R
@@ -1,7 +1,3 @@
-test_that("check_static detects an absence of a file in the extdata", {
-  expect_equal(check_static("abcd/abcd"), FALSE)
-})
-
 test_that("extract_epic gives the specified template", {
   result_extract_epic <- extract_epic("21.T11969/1ea0e148d9bbe08335cd")
   string_representation <- capture.output(print(result_extract_epic))
