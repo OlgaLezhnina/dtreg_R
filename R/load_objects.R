796d9203460df6065a39b1be70b8120f743ff822
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2024-03-30 22:45:04 +0100

    add load_objects and test

---
+++ R/load_objects.R
@@ -0,0 +1,53 @@
+#' Title
+#'
+#' @param template_id The identifier of a DTR template
+#'
+#' @return an R6 object
+#' @export
+#'
+#' @examples
+load_objects <- function(template_id) {
+  datypreg <- Epic$new()
+  datypreg$get_template(template_id)
+  templ_info <- datypreg$template
+  result <- list()
+  for (t in seq_along(templ_info)) {
+    templ_data <- templ_info[[t]][[1]]
+    r6_template <-
+      paste(
+        "R6::R6Class('",
+        paste(format_string(templ_data[[1]]$name), "_epic", sep = ""),
+        "',
+        public = list(
+        name = NULL,
+        identifier = NULL",
+        paste(sprintf(
+          ",\n%s = NULL",
+          format_string(templ_data[[2]]$prop_name)
+        ), collapse = ""),
+        ",
+        initialize = function(
+        name = NA",
+        paste(sprintf(
+          ",\n%s = NA",
+          format_string(templ_data[[2]]$prop_name)
+        ), collapse = ""),
+        ") {
+        self$name = '",
+        format_string(templ_data[[1]]$name),
+        "'
+        self$identifier = '",
+        templ_data[[1]]$identifier,
+        "'",
+        paste(
+          sprintf("\nself$%1$s = %1$s",
+                  format_string(templ_data[[2]]$prop_name)),
+          collapse = ""),
+        "}))",
+        sep = ""
+      )
+    result[[format_string(templ_data[[1]]$name)]] <-
+      eval(parse(text = r6_template))
+  }
+  return(result)
+}
