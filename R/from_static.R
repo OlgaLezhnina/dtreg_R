fad159c47222b10b2e8f1945c9d329358783cef7
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2024-04-01 17:55:15 +0200

    onload experiment-1

--- R/from_static.R
+++ R/from_static.R
@@ -5,7 +5,7 @@
 #'
 check_static <- function(template_doi) {
   part <- strsplit(template_doi, split = "[/ //]+")
-  name <- paste(part[[1]][[3]], part[[1]][[4]], ".json", sep = "")
+  name <- paste(part[[1]][[3]], part[[1]][[4]], "json", sep = ".")
   all_static <-
     system.file("extdata", package = "dtreg") |> list.files()
   checked <- name %in% all_static
--- R/xxx_onload.R
+++ R/xxx_onload.R
@@ -1,4 +1,4 @@
 .onLoad <- function(libname, pkgname) {
   templates <-
-    jsonlite::fromJSON(system.file("extdata", "templates.json", package = "dtreg"))
+    jsonlite::fromJSON(system.file("extdata", "21.T11969.f9bdfd1810b999e3b11e.json", package = "dtreg"))
 }
