513a5dc0f9d67b4aa5bab9e4bd71964884aabc10
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2024-04-06 21:29:47 +0200

    add the$static_all

--- R/from_static.R
+++ R/from_static.R
@@ -7,7 +7,7 @@ from_static <- function(template_doi) {
   part <- strsplit(template_doi, split = "/+")[[1]]
   id <- paste(part[[3]], part[[4]], sep = "/")
   final_template <- NULL
-  for (static in static_all) {
+  for (static in the$static_all) {
     for (templ in static) {
       if (id == templ[[1]][[1]]$identifier) {
         final_template <- list(templ)
