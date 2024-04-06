53219cedc54d6c35bd982638d7874368bdb3f4cf
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2024-04-06 21:24:01 +0200

    improve from_static

--- R/from_static.R
+++ R/from_static.R
@@ -1,30 +1,18 @@
 #' Title
 #'
 #' @param template_doi The DOI of a DTR template
-#' @return The template information from a JSON file, or "none" if not in static
+#' @return The template information from a JSON file, or NULL if not in static
 #'
 from_static <- function(template_doi) {
   part <- strsplit(template_doi, split = "/+")[[1]]
   id <- paste(part[[3]], part[[4]], sep = "/")
   final_template <- NULL
-  for (i in length(TEMPLATES)) {
-    if (id %in% TEMPLATES[[i]]) {
-      static_template <-
-        eval(parse(text = paste("the$template_", i, sep = "")))
-      for (templ in static_template) {
-        if (id == templ[[1]][[1]]$identifier) {
-          final_template <- list(templ)
-        }
+  for (static in static_all) {
+    for (templ in static) {
+      if (id == templ[[1]][[1]]$identifier) {
+        final_template <- list(templ)
       }
     }
   }
   return(final_template)
 }
-
-TEMPLATE_1 <- list("21.T11969/3df63b7acb0522da685d",
-                   "21.T11969/33cc57788c0036ad3bdc",
-                   "21.T11969/f9bdfd1810b999e3b11e")
-TEMPLATE_2 <- list("21.T11969/f76ad9d0324302fc47dd")
-
-TEMPLATES <- list(TEMPLATE_1,
-                  TEMPLATE_2)
--- R/xxx_onload.R
+++ R/xxx_onload.R
@@ -3,4 +3,5 @@
     jsonlite::fromJSON(system.file("extdata", "pidinst_instrumentidentifier.json", package = "dtreg"))
   the$template_2 <-
     jsonlite::fromJSON(system.file("extdata", "pidinst_instrumenttype.json", package = "dtreg"))
+  static_all <- list(the$template_1, the$template_2)
 }
