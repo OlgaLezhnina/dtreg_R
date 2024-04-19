13d3f67d76cde3f8c7a5444a3c914977fd278f53
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2024-04-19 16:38:37 +0200

    lint

--- R/xxx_onload.R
+++ R/xxx_onload.R
@@ -3,8 +3,8 @@
     system.file("extdata", package = "dtreg") |> list.files()
   the$static_all <- list()
   for (name in file_names) {
-    the$static <- list(jsonlite::fromJSON(system.file("extdata", name, package = "dtreg")))
+    the$static <-
+      list(jsonlite::fromJSON(system.file("extdata", name, package = "dtreg")))
     the$static_all[name] <- the$static
   }
 }
-
