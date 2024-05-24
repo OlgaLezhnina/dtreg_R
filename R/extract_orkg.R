4def568e25ace8a1929e746b5b7660b04d610cd8
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2024-05-24 19:08:37 +0200

    add extract_orkg and test

---
+++ R/extract_orkg.R
@@ -0,0 +1,59 @@
+#' Title
+#'
+#' @param dt_id The URL of an ORKG template
+#' @return An R object that contains information about the ORKG template
+#'
+extract_orkg <- function(dt_id) {
+  extract_all <- list()
+  extractor_function <- function(dt_id) {
+    part <- strsplit(dt_id, split = "[/ //]+")[[1]]
+    orkg_prefix <-
+      paste0(part[[1]], "/", part[[2]], "//api/templates/")
+    info <- request_dtr(paste0(orkg_prefix, part[[4]]))
+    dt_name <- info$label
+    dt_id <- info$id
+    dt_class <- info$target_class$id
+    schema_df <- data.frame(dt_name, dt_id, dt_class)
+    i <- 0
+    all_props <- data.frame(
+      dtp_name = character(),
+      dtp_id = character(),
+      dtp_cardinality = character(),
+      dtp_value_type = character(),
+      nested = logical(),
+      stringsAsFactors = FALSE
+    )
+    for (prop in info$properties) {
+      specific_prop <- list()
+      specific_prop[["dtp_name"]] <- prop$path$label
+      specific_prop[["dtp_id"]] <- prop$path$id
+      if (is.null(prop$class$id)) {
+        specific_prop[["dtp_cardinality"]] <- "todo"
+        specific_prop[["dtp_value_type"]] <- prop$datatype$id
+        specific_prop[["nested"]] <- FALSE
+      } else {
+        specific_prop[["dtp_cardinality"]] <- "todo"
+        specific_prop[["dtp_value_type"]] <- prop$class$id
+        info_n <-
+          request_dtr(paste0(orkg_prefix, "?target_class=", prop$class$id))
+        if (!length(info_n$content)) {
+          specific_prop[["nested"]] <- FALSE
+        } else {
+          specific_prop[["nested"]] <- TRUE
+          nested_id <- info_n$content[[1]]$id
+          nested_name <- info_n$content[[1]]$label
+          if (!nested_name %in% names(extract_all)) {
+            extractor_function(nested_id)
+          }
+        }
+        i <- i + 1
+        all_props[i,] <- specific_prop
+      }
+    }
+    extracted <- list(schema_df, all_props)
+    extract_all[[dt_name]] <<- extracted
+    return(extract_all)
+  }
+  extractor_function(dt_id)
+  return(extract_all)
+}
---
+++ tests/testthat/test-extract_orkg.R
@@ -0,0 +1,16 @@
+test_that("extract_orkg gives the specified template", {
+  result_extractor <- extract_orkg("https://incubating.orkg.org/template/R937648")
+  string_representation <- capture.output(print(result_extractor))
+  expected <- c("$measurement_scale",
+                "$measurement_scale[[1]]",
+                "            dt_name   dt_id dt_class",
+                "1 measurement_scale R937648   C75002",
+                "",
+                "$measurement_scale[[2]]",
+                "[1] dtp_name        dtp_id          dtp_cardinality dtp_value_type ",
+                "[5] nested         ",
+                "<0 rows> (or 0-length row.names)",
+                "",
+                 "")
+  expect_equal(string_representation, expected)
+})
