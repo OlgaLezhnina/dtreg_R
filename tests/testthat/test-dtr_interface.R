67973e6ac315ae4e362971f13d216d3473d8c8f6
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2024-07-06 09:12:51 +0200

    include add_dtp_type in the test

--- tests/testthat/test-dtr_interface.R
+++ tests/testthat/test-dtr_interface.R
@@ -1,11 +1,13 @@
 test_that("select_dtr creates an ePIC object for an ePIC template_doi", {
   templ <- select_dtr("https://doi.org/21.T11969/1ea0e148d9bbe08335cd")
-  expected <- c(".__enclos_env__", "template_info",        "clone",  "add_dt_type",  "add_context",       "get_template_info",    "initialize")
+  expected <- c(".__enclos_env__", "template_info", "clone", "add_dtp_type",
+                "add_dt_type",  "add_context", "get_template_info", "initialize")
   expect_equal(names(templ$new()), expected)
 })
 
 test_that("select_dtr creates an ORKG object for an ORKG template URL", {
   templ <- select_dtr("https://incubating.orkg.org/template/R937648")
-  expected <- c(".__enclos_env__", "template_info",        "clone",     "add_dt_type",       "add_context", "get_template_info", "initialize")
+  expected <- c(".__enclos_env__", "template_info", "clone", "add_dtp_type",
+                "add_dt_type", "add_context", "get_template_info", "initialize")
   expect_equal(names(templ$new()), expected)
 })
