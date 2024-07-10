4b27f734d633e04067b66400678d46c72ca4883d
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2024-07-10 20:31:31 +0200

    rename and document

--- R/load_datatype.R
+++ R/load_datatype.R
@@ -1,55 +1,55 @@
-#' Title
+#' Load datatype
 #'
-#' @param template_doi The DOI of a DTR template
+#' @param datatype_id The identifier of a datatype
 #'
-#' @return a list of objects for creating new instances
+#' @return a list of schemata for creating new instances
 #' @export
 #'
 #' @examples
 #' pd <- load_datatype("https://doi.org/21.T11969/1ea0e148d9bbe08335cd")
 #'
-load_datatype <- function(template_doi) {
-  r6_objects <- write_r6_classes(template_doi)
-  result <- write_proxies(r6_objects)
-  return(result)
+load_datatype <- function(datatype_id) {
+  r6_classes <- write_r6_classes(datatype_id)
+  schemata <- write_proxies(r6_classes)
+  return(schemata)
 }
 
-#' Title
+#' Write proxies
 #'
-#' @param object_list A list of R6 objects
+#' @param r6_classes A list of R6 classes for a specified datatype
 #' @return a list of objects for creating new instances
 #'
-write_proxies <- function(object_list) {
+write_proxies <- function(r6_classes) {
   proxies <- list()
-  for (object in object_list) {
+  for (object in r6_classes) {
     name <- stringr::str_sub(object$classname, end = -4)
     proxies[[name]] <- object$new
   }
   return(proxies)
 }
 
-#' Title
+#' Write R6 classes for a specified datatype
 #'
-#' @param template_doi The DOI of a DTR template
-#' @return an R6 object for the template
+#' @param datatype_id The identifier of a datatype
+#' @return a list of R6 classes for the datatype
 #'
-write_r6_classes <- function(template_doi) {
-  selected_class <- select_dtr(template_doi)
-  templ_info <- selected_class$new()$get_template_info(template_doi)
-  result <- list()
-  for (t in seq_along(templ_info)) {
-    templ_data <- templ_info[[t]]
-    r6_template <-
+write_r6_classes <- function(datatype_id) {
+  selected_class <- select_dtr(datatype_id)
+  schema_info <- selected_class$new()$get_schema_info(datatype_id)
+  all_classes <- list()
+  for (t in seq_along(schema_info)) {
+    schema_data <- schema_info[[t]]
+    r6_class <-
       R6::R6Class(
-        paste0(templ_data[[1]]$dt_name, "_r6"),
+        paste0(schema_data[[1]]$dt_name, "_r6"),
         inherit = selected_class,
         public = c(
           list(
             prefix = get_prefix(template_doi),
-            dt_name = templ_data[[1]]$dt_name,
-            dt_id = templ_data[[1]]$dt_id,
-            prop_names = templ_data[[2]]$dtp_name,
-            prop_info = templ_data[[2]],
+            dt_name = schema_data[[1]]$dt_name,
+            dt_id = schema_data[[1]]$dt_id,
+            prop_names = schema_data[[2]]$dtp_name,
+            prop_info = schema_data[[2]],
             initialize = function(...) {
               args <- list(...)
               for (dtp_name in self$prop_names) {
@@ -57,11 +57,11 @@ write_r6_classes <- function(template_doi) {
               }
             }
           ),
-          sapply(templ_data[[2]]$dtp_name, function(x)
+          sapply(schema_data[[2]]$dtp_name, function(x)
             NULL)
         )
       )
-    result[[format_string(templ_data[[1]]$dt_name)]] <- r6_template
+    all_classes[[format_string(schema_data[[1]]$dt_name)]] <- r6_class
   }
-  return(result)
+  return(all_classes)
 }
