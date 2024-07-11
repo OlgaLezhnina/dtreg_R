2ad2ffc642052b0f81185e6d31dc7c79d617edb6
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2024-07-11 18:44:08 +0200

    more unit tests

--- tests/testthat/test-helpers.R
+++ tests/testthat/test-helpers.R
@@ -2,6 +2,14 @@ test_that("format_string makes text lowcase, spaces and dashes to underscores", 
   expect_equal(format_string("a-B c"), "a_b_c")
 })
 
+test_that("get_prefix returns the epic prefix", {
+  expect_equal(get_prefix("https://doi.org/21.T11969/74bc7748b8cd520908bc"), "https://doi.org/21.T11969/")
+})
+
+test_that("get_prefix returns the orkg prefix", {
+  expect_equal(get_prefix("https://incubating.orkg.org/template/R855534"), "https://incubating.orkg.org/")
+})
+
 test_that("specify_cardinality outputs the specific list for min_max", {
   expect_equal(specify_cardinality("0 - 1"), list(min = 0, max = 1))
 })
--- tests/testthat/test-load_datatype.R
+++ tests/testthat/test-load_datatype.R
@@ -2,3 +2,10 @@ test_that("load_datatype loads a specific ePIC schema", {
   schema <- load_datatype("https://doi.org/21.T11969/1ea0e148d9bbe08335cd")
   expect_equal(names(schema), "pidinst_schemaobject")
 })
+
+test_that("load_datatype loads ePIC schema properties correctly", {
+  dt <- load_datatype("https://doi.org/21.T11969/74bc7748b8cd520908bc")
+  prop_names <- dt$inferential_test_output()$prop_names
+  expected <- c("label", "has_description", "comment", "has_format")
+  expect_equal(prop_names, expected)
+})
