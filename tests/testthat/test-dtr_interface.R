527967a399e014931792d085616c88a931375e3e
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2024-07-11 20:42:32 +0200

    more unit tests

--- tests/testthat/test-dtr_interface.R
+++ tests/testthat/test-dtr_interface.R
@@ -1,13 +1,18 @@
-test_that("select_dtr creates an ePIC object for an ePIC template_doi", {
-  templ <- select_dtr("https://doi.org/21.T11969/1ea0e148d9bbe08335cd")
+test_that("select_dtr selects class ePIC object for an ePIC schema URL", {
+  schema <- select_dtr("https://doi.org/21.T11969/1ea0e148d9bbe08335cd")
   expected <- c(".__enclos_env__", "schema_info", "clone", "add_df_constants", "add_dtp_type",
                 "add_dt_type",  "add_context", "get_schema_info", "initialize")
-  expect_equal(names(templ$new()), expected)
+  expect_equal(names(schema$new()), expected)
 })
 
-test_that("select_dtr creates an ORKG object for an ORKG template URL", {
-  templ <- select_dtr("https://incubating.orkg.org/template/R937648")
+test_that("select_dtr selects class ORKG for an ORKG template URL", {
+  schema <- select_dtr("https://incubating.orkg.org/template/R937648")
   expected <- c(".__enclos_env__", "schema_info", "clone", "add_df_constants", "add_dtp_type",
                 "add_dt_type", "add_context", "get_schema_info", "initialize")
-  expect_equal(names(templ$new()), expected)
+  expect_equal(names(schema$new()), expected)
+})
+
+test_that("select_dtr gives error when the URL is neither ePIC nor ORKG", {
+  wrong_URL <- "https://stackoverflow.com/questions"
+  expect_error(select_dtr(wrong_URL), "Please check whether the schema belongs to the ePIC or the ORKG dtr", fixed = TRUE)
 })
--- tests/testthat/test-to_jsonld.R
+++ tests/testthat/test-to_jsonld.R
@@ -2,14 +2,14 @@ test_that("differ_input returns the list for a numeric input", {
   expect_equal(differ_input(3), 3)
 })
 
-test_that("df_structure writes a dataframe in a specified way", {
+test_that("differ_input calls the df_structure for a dataframe", {
   df <- data.frame(A = 1, B = 2, stringsAsFactors = FALSE)
   the$uid <- generate_uid()
-  result <- df_structure(df, label = "Table")
-  expect_equal(result$columns[[1]]$col_titles, "A")
+  result <- differ_input(df)
+  expect_equal(result$rows[[1]]$cells[[1]]$value, "1")
 })
 
-test_that("df_structure writes a tuple in a specified way", {
+test_that("df_structure writes a dataframe in a specified way", {
   df <- data.frame(A = 1, B = 2, stringsAsFactors = FALSE)
   the$uid <- generate_uid()
   result <- df_structure(df, label = "Table")
