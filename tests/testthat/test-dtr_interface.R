bd613d8c58d9336207e6fbe8c29bf10d9f6a98ba
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2024-07-14 17:25:46 +0200

    improve tests

--- tests/testthat/test-dtr_interface.R
+++ tests/testthat/test-dtr_interface.R
@@ -1,15 +1,11 @@
 test_that("select_dtr selects class ePIC object for an ePIC schema URL", {
   schema <- select_dtr("https://doi.org/21.T11969/1ea0e148d9bbe08335cd")
-  expected <- c(".__enclos_env__", "schema_info", "clone", "add_df_constants", "add_dtp_type",
-                "add_dt_type",  "add_context", "get_schema_info", "initialize")
-  expect_equal(names(schema$new()), expected)
+  expect_equal(schema$classname, "Epic")
 })
 
 test_that("select_dtr selects class ORKG for an ORKG template URL", {
   schema <- select_dtr("https://incubating.orkg.org/template/R937648")
-  expected <- c(".__enclos_env__", "schema_info", "clone", "add_df_constants", "add_dtp_type",
-                "add_dt_type", "add_context", "get_schema_info", "initialize")
-  expect_equal(names(schema$new()), expected)
+  expect_equal(schema$classname, "Orkg")
 })
 
 test_that("select_dtr gives error when the URL is neither ePIC nor ORKG", {
