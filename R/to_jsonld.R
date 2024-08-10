224cf58709e650f97792e9fc20dd4b4de58bc976
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2024-08-10 12:48:09 +0200

    remove the dt_name key from jsonld

--- R/to_jsonld.R
+++ R/to_jsonld.R
@@ -37,7 +37,7 @@ to_jsonld <- function(instance) {
     return(result)
   }
   result_all <- list()
-  result_all[[instance$dt_name]] <- write_info(instance)
+  result_all <- write_info(instance)
   result_all[["@context"]] <- instance$add_context(instance$prefix)
   inst_json <-
     jsonlite::toJSON(result_all, pretty = TRUE, auto_unbox = TRUE)
