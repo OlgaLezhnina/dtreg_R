2a439eb46b02f32cccc71f8d86ff20e31d6879af
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2024-09-01 11:37:55 +0200

    check that input is not a function

--- R/to_jsonld.R
+++ R/to_jsonld.R
@@ -24,6 +24,8 @@ to_jsonld <- function(instance) {
       prop_type <- instance$add_dtp_type(prop_id)
       if (is.null(instance_field)) {
         next
+      } else if (is.function(instance_field)) {
+        stop(paste("Input in ", field, " should not be a function"))
       } else if (is.list(instance_field) &&
                    inherits(instance_field[[1]], "R6")) {
         result[[prop_type]] <- lapply(instance_field, write_info)
