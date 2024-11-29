14d997811e1201b780a474748e534f3a8781ef19
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2024-11-29 20:05:43 +0100

    add a unit test for nested

--- tests/testthat/test-to_jsonld.R
+++ tests/testthat/test-to_jsonld.R
@@ -97,7 +97,37 @@ test_that("to_jsonld writes an ePIC instance into JSONLD", {
   expect_equal(capture.output(print(result)), expected)
 })
 
-test_that("to_jsonld writes an ePIC instance with a list of nested component", {
+test_that("to_jsonld writes an ePIC instance with a nested component", {
+  dt <- load_datatype("https://doi.org/21.T11969/aff130c76e68ead3862e")
+  url <- dt$url()
+  instance <- dt$data_item(has_expression = url)
+  result <- to_jsonld(instance)
+  expected <- c("{",
+                "  \"@id\": \"_:n1\",",
+                "  \"@type\": \"doi:aff130c76e68ead3862e\",",
+                "  \"doi:aff130c76e68ead3862e#has_expression\": {",
+                "    \"@id\": \"_:n2\",",
+                "    \"@type\": \"doi:e0efc41346cda4ba84ca\"",
+                "  },",
+                "  \"@context\": {",
+                "    \"doi\": \"https://doi.org/21.T11969/\",",
+                "    \"columns\": \"https://doi.org/21.T11969/0424f6e7026fa4bc2c4a#columns\",",
+                "    \"col_number\": \"https://doi.org/21.T11969/65ba00e95e60fb8971e6#number\",",
+                "    \"col_titles\": \"https://doi.org/21.T11969/65ba00e95e60fb8971e6#titles\",",
+                "    \"rows\": \"https://doi.org/21.T11969/0424f6e7026fa4bc2c4a#rows\",",
+                "    \"row_number\": \"https://doi.org/21.T11969/9bf7a8e8909bfd491b38#number\",",
+                "    \"row_titles\": \"https://doi.org/21.T11969/9bf7a8e8909bfd491b38#titles\",",
+                "    \"cells\": \"https://doi.org/21.T11969/9bf7a8e8909bfd491b38#cells\",",
+                "    \"column\": \"https://doi.org/21.T11969/4607bc7c42ac8db29bfc#column\",",
+                "    \"value\": \"https://doi.org/21.T11969/4607bc7c42ac8db29bfc#value\",",
+                "    \"tab_label\": \"https://doi.org/21.T11969/0424f6e7026fa4bc2c4a#label\"",
+                "  }",
+                "} ")
+
+  expect_equal(capture.output(print(result)), expected)
+})
+
+test_that("to_jsonld writes an ePIC instance with a list of nested components", {
   dt <- load_datatype("https://doi.org/21.T11969/aff130c76e68ead3862e")
   url_1 <- dt$url()
   url_2 <- dt$url()
